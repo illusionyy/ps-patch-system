b73790ff8922b5809c6c995a96ee6c3f693f5367
illusiony <37698908+illusionyy@users.noreply.github.com>
2026-07-06 08:54:50 +0000

    sys_thr_get_name: enforce 32 bytes limit

--- shared/proc.h
+++ shared/proc.h
@@ -3,8 +3,14 @@
 #include <sys/cdefs.h>
 #include <unistd.h>
 
+#define PROC_BUFF_N 32
+
 __BEGIN_DECLS
-int sys_thr_get_name(const int pid, char* buf, const unsigned long nbuf);
+/**
+ * Retrieves thread name of given pid, max size for buf is 32 bytes,
+ * any larger buf will be truncated to 31 characters + null term.
+ **/
+int sys_thr_get_name(const int pid, char buf[PROC_BUFF_N]);
 /**
  * Find the pid of a process with the given name. If multiple processes share
  * the same name, the one with the lowest pid is returned.
